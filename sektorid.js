fe107d684250df89069cd8402ec4d7fca1308789
annegretepeek <annegretepeek@gmail.com>
2018-05-02 10:22:21 +0300

    initial commit

---
+++ sektorid.js
@@ -0,0 +1,1201 @@
+var sektorid = [ 	
+	{
+		"Sektor": "Avalik haldus ja riigikaitse; kohustuslik sotsiaalkindlustus",
+		"values": [{
+			"Aasta": 2007,
+			"Koht": 3,
+			"Tootajad": 34776
+
+		},
+		{
+			"Aasta": 2008,
+			"Koht": 3,
+			"Tootajad": 34776
+
+		},
+		{
+			"Aasta": 2009,
+			"Koht": 4,
+			"Tootajad": 34693
+		},
+		{
+			"Aasta": 2010,
+			"Koht": 6,
+			"Tootajad": 34935
+		},
+		{
+			"Aasta": 2011,
+			"Koht": 6,
+			"Tootajad": 35034
+		},
+		{
+			"Aasta": 2012,
+			"Koht": 6,
+			"Tootajad": 35138
+		},
+		{
+			"Aasta": 2013,
+			"Koht": 6,
+			"Tootajad": 34552
+		},
+		{
+			"Aasta": 2014,
+			"Koht": 6,
+			"Tootajad": 34624
+		},
+		{
+			"Aasta": 2015,
+			"Koht": 5,
+			"Tootajad": 34792
+		},
+		{
+			"Aasta": 2016,
+			"Koht": 4,
+			"Tootajad": 34402
+		},
+		{
+			"Aasta": 2017,
+			"Koht": 5,
+			"Tootajad": 33251
+		},
+		{
+			"Aasta": 2018,
+			"Koht": 5,
+			"Tootajad": 33251
+		}]
+	},
+    {
+        "Sektor": "Ehitus",
+		"values": [{
+			"Aasta": 2007,
+			"Koht": 7,
+			"Tootajad": 50447
+		},
+		{
+			"Aasta": 2008,
+			"Koht": 7,
+			"Tootajad": 50447
+		},
+		{
+			"Aasta": 2009,
+			"Koht": 10,
+			"Tootajad": 39462
+		},
+		{
+			"Aasta": 2010,
+			"Koht": 10,
+			"Tootajad": 34961
+		},
+		{
+			"Aasta": 2011,
+			"Koht": 7,
+			"Tootajad": 32019
+		},
+		{
+			"Aasta": 2012,
+			"Koht": 7,
+			"Tootajad": 35869
+		},
+		{
+			"Aasta": 2013,
+			"Koht": 7,
+			"Tootajad": 34366
+		},
+		{
+			"Aasta": 2014,
+			"Koht": 7,
+			"Tootajad": 35907
+		},
+		{
+			"Aasta": 2015,
+			"Koht": 10,
+			"Tootajad": 33535
+		},
+		{
+			"Aasta": 2016,
+			"Koht": 10,
+			"Tootajad": 32182
+		},
+		{
+			"Aasta": 2017,
+			"Koht": 11,
+			"Tootajad": 35188
+		},
+		{
+			"Aasta": 2018,
+			"Koht": 11,
+			"Tootajad": 35188
+		}]
+	},
+    {
+        "Sektor": "Elektrienergia, gaasi, auru ja konditsioneeritud õhuga varustamine",
+		"values": [{
+			"Aasta": 2007,
+			"Koht": 4,
+			"Tootajad": 6041
+		},
+		{
+			"Aasta": 2008,
+			"Koht": 4,
+			"Tootajad": 6041
+		},
+		{
+			"Aasta": 2009,
+			"Koht": 3,
+			"Tootajad": 5688
+		},
+		{
+			"Aasta": 2010,
+			"Koht": 3,
+			"Tootajad": 5446
+		},
+		{
+			"Aasta": 2011,
+			"Koht": 3,
+			"Tootajad": 5278
+		},
+		{
+			"Aasta": 2012,
+			"Koht": 3,
+			"Tootajad": 5034
+		},
+		{
+			"Aasta": 2013,
+			"Koht": 3,
+			"Tootajad": 5126
+		},
+		{
+			"Aasta": 2014,
+			"Koht": 3,
+			"Tootajad": 4862
+		},
+		{
+			"Aasta": 2015,
+			"Koht": 3,
+			"Tootajad": 4699
+		},
+		{
+			"Aasta": 2016,
+			"Koht": 3,
+			"Tootajad": 4499
+		},
+		{
+			"Aasta": 2017,
+			"Koht": 3,
+			"Tootajad": 4925
+		},
+		{
+			"Aasta": 2018,
+			"Koht": 3,
+			"Tootajad": 4925
+		}]
+	},
+    {
+        "Sektor": "Finants- ja kindlustustegevus",
+		"values": [{
+			"Aasta": 2007,
+			"Koht": 1,
+			"Tootajad": 8832
+		},
+		{
+			"Aasta": 2008,
+			"Koht": 1,
+			"Tootajad": 8832
+		},
+		{
+			"Aasta": 2009,
+			"Koht": 1,
+			"Tootajad": 8579
+		},
+		{
+			"Aasta": 2010,
+			"Koht": 1,
+			"Tootajad": 8500
+		},
+		{
+			"Aasta": 2011,
+			"Koht": 2,
+			"Tootajad": 8704
+		},
+		{
+			"Aasta": 2012,
+			"Koht": 2,
+			"Tootajad": 8409
+		},
+		{
+			"Aasta": 2013,
+			"Koht": 2,
+			"Tootajad": 8124
+		},
+		{
+			"Aasta": 2014,
+			"Koht": 1,
+			"Tootajad": 7889
+		},
+		{
+			"Aasta": 2015,
+			"Koht": 1,
+			"Tootajad": 8691
+		},
+		{
+			"Aasta": 2016,
+			"Koht": 2,
+			"Tootajad": 9589
+		},
+		{
+			"Aasta": 2017,
+			"Koht": 2,
+			"Tootajad": 9841
+		},
+		{
+			"Aasta": 2018,
+			"Koht": 2,
+			"Tootajad": 9841
+		}]
+	},
+    {
+        "Sektor": "Haldus- ja abitegevused",
+		"values": [{
+			"Aasta": 2007,
+			"Koht": 13,
+			"Tootajad": 24418
+		},
+		{
+			"Aasta": 2008,
+			"Koht": 13,
+			"Tootajad": 24418
+		},
+		{
+			"Aasta": 2009,
+			"Koht": 13,
+			"Tootajad": 23222
+		},
+		{
+			"Aasta": 2010,
+			"Koht": 12,
+			"Tootajad": 22158
+		},
+		{
+			"Aasta": 2011,
+			"Koht": 13,
+			"Tootajad": 25400
+		},
+		{
+			"Aasta": 2012,
+			"Koht": 13,
+			"Tootajad": 26822
+		},
+		{
+			"Aasta": 2013,
+			"Koht": 13,
+			"Tootajad": 26849
+		},
+		{
+			"Aasta": 2014,
+			"Koht": 14,
+			"Tootajad": 27910
+		},
+		{
+			"Aasta": 2015,
+			"Koht": 15,
+			"Tootajad": 25410
+		},
+		{
+			"Aasta": 2016,
+			"Koht": 13,
+			"Tootajad": 24315
+		},
+		{
+			"Aasta": 2017,
+			"Koht": 12,
+			"Tootajad": 28168
+		},
+		{
+			"Aasta": 2018,
+			"Koht": 12,
+			"Tootajad": 28168
+		}]
+	},
+    {
+        "Sektor": "Haridus",
+		"values": [{
+			"Aasta": 2007,
+			"Koht": 14,
+			"Tootajad": 51123
+		},
+		{
+			"Aasta": 2008,
+			"Koht": 14,
+			"Tootajad": 51123
+		},
+		{
+			"Aasta": 2009,
+			"Koht": 14,
+			"Tootajad": 49806
+		},
+		{
+			"Aasta": 2010,
+			"Koht": 14,
+			"Tootajad": 49056
+		},
+		{
+			"Aasta": 2011,
+			"Koht": 14,
+			"Tootajad": 48582
+		},
+		{
+			"Aasta": 2012,
+			"Koht": 15,
+			"Tootajad": 48574
+		},
+		{
+			"Aasta": 2013,
+			"Koht": 15,
+			"Tootajad": 48486
+		},
+		{
+			"Aasta": 2014,
+			"Koht": 15,
+			"Tootajad": 48170
+		},
+		{
+			"Aasta": 2015,
+			"Koht": 14,
+			"Tootajad": 48170
+		},
+		{
+			"Aasta": 2016,
+			"Koht": 15,
+			"Tootajad": 47765
+		},
+		{
+			"Aasta": 2017,
+			"Koht": 14,
+			"Tootajad": 48138
+		},
+		{
+			"Aasta": 2018,
+			"Koht": 14,
+			"Tootajad": 48138
+		}]
+	},
+    {
+        "Sektor": "Hulgi- ja jaekaubandus; mootorsõidukite ja mootorrataste remont",
+		"values": [{
+			"Aasta": 2007,
+			"Koht": 11,
+			"Tootajad": 90184
+		},
+		{
+			"Aasta": 2008,
+			"Koht": 11,
+			"Tootajad": 90184
+		},
+		{
+			"Aasta": 2009,
+			"Koht": 11,
+			"Tootajad": 83912
+		},
+		{
+			"Aasta": 2010,
+			"Koht": 13,
+			"Tootajad": 75149
+		},
+		{
+			"Aasta": 2011,
+			"Koht": 12,
+			"Tootajad": 74237
+		},
+		{
+			"Aasta": 2012,
+			"Koht": 12,
+			"Tootajad": 78338
+		},
+		{
+			"Aasta": 2013,
+			"Koht": 12,
+			"Tootajad": 75360
+		},
+		{
+			"Aasta": 2014,
+			"Koht": 12,
+			"Tootajad": 83069
+		},
+		{
+			"Aasta": 2015,
+			"Koht": 12,
+			"Tootajad": 79237
+		},
+		{
+			"Aasta": 2016,
+			"Koht": 14,
+			"Tootajad": 79859
+		},
+		{
+			"Aasta": 2017,
+			"Koht": 13,
+			"Tootajad": 81568
+		},
+		{
+			"Aasta": 2018,
+			"Koht": 13,
+			"Tootajad": 81568
+		}]
+	},
+    {
+        "Sektor": "Info ja side",
+		"values": [{
+			"Aasta": 2007,
+			"Koht": 2,
+			"Tootajad": 14867
+		},
+		{
+			"Aasta": 2008,
+			"Koht": 2,
+			"Tootajad": 14867
+		},
+		{
+			"Aasta": 2009,
+			"Koht": 2,
+			"Tootajad": 14867
+		},
+		{
+			"Aasta": 2010,
+			"Koht": 2,
+			"Tootajad": 14867
+		},
+		{
+			"Aasta": 2011,
+			"Koht": 1,
+			"Tootajad": 14867
+		},
+		{
+			"Aasta": 2012,
+			"Koht": 1,
+			"Tootajad": 14867
+		},
+		{
+			"Aasta": 2013,
+			"Koht": 1,
+			"Tootajad": 14867
+		},
+		{
+			"Aasta": 2014,
+			"Koht": 2,
+			"Tootajad": 15722
+		},
+		{
+			"Aasta": 2015,
+			"Koht": 2,
+			"Tootajad": 17155
+		},
+		{
+			"Aasta": 2016,
+			"Koht": 1,
+			"Tootajad": 17816
+		},
+		{
+			"Aasta": 2017,
+			"Koht": 1,
+			"Tootajad": 20099
+		},
+		{
+			"Aasta": 2018,
+			"Koht": 1,
+			"Tootajad": 20099
+		}]
+	},
+    {
+        "Sektor": "Kinnisvaraalane tegevus",
+		"values": [{
+			"Aasta": 2007,
+			"Koht": 16,
+			"Tootajad": 7215
+		},
+		{
+			"Aasta": 2008,
+			"Koht": 16,
+			"Tootajad": 7215
+		},
+		{
+			"Aasta": 2009,
+			"Koht": 16,
+			"Tootajad": 5915
+		},
+		{
+			"Aasta": 2010,
+			"Koht": 17,
+			"Tootajad": 4779
+		},
+		{
+			"Aasta": 2011,
+			"Koht": 17,
+			"Tootajad": 5826
+		},
+		{
+			"Aasta": 2012,
+			"Koht": 17,
+			"Tootajad": 5529
+		},
+		{
+			"Aasta": 2013,
+			"Koht": 17,
+			"Tootajad": 5584
+		},
+		{
+			"Aasta": 2014,
+			"Koht": 17,
+			"Tootajad": 6166
+		},
+		{
+			"Aasta": 2015,
+			"Koht": 17,
+			"Tootajad": 5368
+		},
+		{
+			"Aasta": 2016,
+			"Koht": 16,
+			"Tootajad": 4780
+		},
+		{
+			"Aasta": 2017,
+			"Koht": 16,
+			"Tootajad": 5787
+		},
+		{
+			"Aasta": 2018,
+			"Koht": 16,
+			"Tootajad": 5787
+		}]
+	},
+    {
+        "Sektor": "Kunst, meelelahutus ja vaba aeg",
+		"values": [{
+			"Aasta": 2007,
+			"Koht": 17,
+			"Tootajad": 11571
+		},
+		{
+			"Aasta": 2008,
+			"Koht": 17,
+			"Tootajad": 11571
+		},
+		{
+			"Aasta": 2009,
+			"Koht": 17,
+			"Tootajad": 10596
+		},
+		{
+			"Aasta": 2010,
+			"Koht": 16,
+			"Tootajad": 9006
+		},
+		{
+			"Aasta": 2011,
+			"Koht": 16,
+			"Tootajad": 9076
+		},
+		{
+			"Aasta": 2012,
+			"Koht": 16,
+			"Tootajad": 9569
+		},
+		{
+			"Aasta": 2013,
+			"Koht": 16,
+			"Tootajad": 9001
+		},
+		{
+			"Aasta": 2014,
+			"Koht": 16,
+			"Tootajad": 9063
+		},
+		{
+			"Aasta": 2015,
+			"Koht": 16,
+			"Tootajad": 9289
+		},
+		{
+			"Aasta": 2016,
+			"Koht": 17,
+			"Tootajad": 8924
+		},
+		{
+			"Aasta": 2017,
+			"Koht": 17,
+			"Tootajad": 9254
+		},
+		{
+			"Aasta": 2018,
+			"Koht": 17,
+			"Tootajad": 9254
+		}]
+	},
+    {
+        "Sektor": "Kutse-, teadus- ja tehnikaalane tegevus",
+		"values": [{
+			"Aasta": 2007,
+			"Koht": 5,
+			"Tootajad": 16977
+		},
+		{
+			"Aasta": 2008,
+			"Koht": 5,
+			"Tootajad": 16977
+		},
+		{
+			"Aasta": 2009,
+			"Koht": 5,
+			"Tootajad": 17163
+		},
+		{
+			"Aasta": 2010,
+			"Koht": 4,
+			"Tootajad": 15797
+		},
+		{
+			"Aasta": 2011,
+			"Koht": 5,
+			"Tootajad": 16990
+		},
+		{
+			"Aasta": 2012,
+			"Koht": 4,
+			"Tootajad": 19468
+		},
+		{
+			"Aasta": 2013,
+			"Koht": 5,
+			"Tootajad": 18031
+		},
+		{
+			"Aasta": 2014,
+			"Koht": 5,
+			"Tootajad": 19594
+		},
+		{
+			"Aasta": 2015,
+			"Koht": 6,
+			"Tootajad": 17985
+		},
+		{
+			"Aasta": 2016,
+			"Koht": 6,
+			"Tootajad": 18898
+		},
+		{
+			"Aasta": 2017,
+			"Koht": 6,
+			"Tootajad": 18272
+		},
+		{
+			"Aasta": 2018,
+			"Koht": 6,
+			"Tootajad": 18272
+		}]
+	},
+    {
+        "Sektor": "Majutus ja toitlustus",
+		"values": [{
+				"Aasta": 2007,
+				"Koht": 19,
+				"Tootajad": 17107
+			},
+			{
+				"Aasta": 2008,
+				"Koht": 19,
+				"Tootajad": 17107
+			},
+			{
+				"Aasta": 2009,
+				"Koht": 19,
+				"Tootajad": 14443
+			},
+			{
+				"Aasta": 2010,
+				"Koht": 18,
+				"Tootajad": 14161
+			},
+			{
+				"Aasta": 2011,
+				"Koht": 18,
+				"Tootajad": 14321
+			},
+			{
+				"Aasta": 2012,
+				"Koht": 18,
+				"Tootajad": 15239
+			},
+			{
+				"Aasta": 2013,
+				"Koht": 18,
+				"Tootajad": 15466
+			},
+			{
+				"Aasta": 2014,
+				"Koht": 18,
+				"Tootajad": 17982
+			},
+			{
+				"Aasta": 2015,
+				"Koht": 18,
+				"Tootajad": 16673
+			},
+			{
+				"Aasta": 2016,
+				"Koht": 18,
+				"Tootajad": 15562
+			},
+			{
+				"Aasta": 2017,
+				"Koht": 18,
+				"Tootajad": 17384
+			},
+			{
+				"Aasta": 2018,
+				"Koht": 18,
+				"Tootajad": 17384
+			}]
+		},
+    {
+        "Sektor": "Muud teenindavad tegevused",
+		"values": [{
+			"Aasta": 2007,
+			"Koht": 18,
+			"Tootajad": 3451
+		},
+		{
+			"Aasta": 2008,
+			"Koht": 18,
+			"Tootajad": 3451
+		},
+		{
+			"Aasta": 2009,
+			"Koht": 18,
+			"Tootajad": 3897
+		},
+		{
+			"Aasta": 2010,
+			"Koht": 19,
+			"Tootajad": 3176
+		},
+		{
+			"Aasta": 2011,
+			"Koht": 19,
+			"Tootajad": 3224
+		},
+		{
+			"Aasta": 2012,
+			"Koht": 19,
+			"Tootajad": 3694
+		},
+		{
+			"Aasta": 2013,
+			"Koht": 19,
+			"Tootajad": 4002
+		},
+		{
+			"Aasta": 2014,
+			"Koht": 19,
+			"Tootajad": 4102
+		},
+		{
+			"Aasta": 2015,
+			"Koht": 19,
+			"Tootajad": 3791
+		},
+		{
+			"Aasta": 2016,
+			"Koht": 19,
+			"Tootajad": 3033
+		},
+		{
+			"Aasta": 2017,
+			"Koht": 19,
+			"Tootajad": 3200
+		},
+		{
+			"Aasta": 2018,
+			"Koht": 19,
+			"Tootajad": 3200
+		}]
+	},
+    {
+        "Sektor": "Mäetööstus",
+		"values": [{
+			"Aasta": 2007,
+			"Koht": 6,
+			"Tootajad": 4933
+		},
+		{
+			"Aasta": 2008,
+			"Koht": 6,
+			"Tootajad": 4933
+		},
+		{
+			"Aasta": 2009,
+			"Koht": 6,
+			"Tootajad": 4748
+		},
+		{
+			"Aasta": 2010,
+			"Koht": 5,
+			"Tootajad": 4662
+		},
+		{
+			"Aasta": 2011,
+			"Koht": 4,
+			"Tootajad": 4877
+		},
+		{
+			"Aasta": 2012,
+			"Koht": 5,
+			"Tootajad": 5041
+		},
+		{
+			"Aasta": 2013,
+			"Koht": 4,
+			"Tootajad": 4961
+		},
+		{
+			"Aasta": 2014,
+			"Koht": 4,
+			"Tootajad": 4787
+		},
+		{
+			"Aasta": 2015,
+			"Koht": 4,
+			"Tootajad": 4657
+		},
+		{
+			"Aasta": 2016,
+			"Koht": 5,
+			"Tootajad": 4113
+		},
+		{
+			"Aasta": 2017,
+			"Koht": 4,
+			"Tootajad": 4340
+		},
+		{
+			"Aasta": 2018,
+			"Koht": 4,
+			"Tootajad": 4340
+		}]
+	},
+    {
+        "Sektor": "Põllumajandus, metsamajandus ja kalapüük",
+		"values": [{
+			"Aasta": 2007,
+			"Koht": 15,
+			"Tootajad": 15235
+		},
+		{
+			"Aasta": 2008,
+			"Koht": 15,
+			"Tootajad": 15235
+		},
+		{
+			"Aasta": 2009,
+			"Koht": 15,
+			"Tootajad": 11064
+		},
+		{
+			"Aasta": 2010,
+			"Koht": 15,
+			"Tootajad": 12399
+		},
+		{
+			"Aasta": 2011,
+			"Koht": 15,
+			"Tootajad": 11543
+		},
+		{
+			"Aasta": 2012,
+			"Koht": 14,
+			"Tootajad": 11838
+		},
+		{
+			"Aasta": 2013,
+			"Koht": 14,
+			"Tootajad": 13424
+		},
+		{
+			"Aasta": 2014,
+			"Koht": 13,
+			"Tootajad": 15042
+		},
+		{
+			"Aasta": 2015,
+			"Koht": 13,
+			"Tootajad": 14129
+		},
+		{
+			"Aasta": 2016,
+			"Koht": 12,
+			"Tootajad": 13604
+		},
+		{
+			"Aasta": 2017,
+			"Koht": 15,
+			"Tootajad": 15498
+		},
+		{
+			"Aasta": 2018,
+			"Koht": 15,
+			"Tootajad": 15498
+		}]
+	},
+    {
+        "Sektor": "Tervishoid ja sotsiaalhoolekanne",
+		"values": [{
+			"Aasta": 2007,
+			"Koht": 8,
+			"Tootajad": 29863
+		},
+		{
+			"Aasta": 2008,
+			"Koht": 8,
+			"Tootajad": 29863
+		},
+		{
+			"Aasta": 2009,
+			"Koht": 7,
+			"Tootajad": 31014
+		},
+		{
+			"Aasta": 2010,
+			"Koht": 9,
+			"Tootajad": 29584
+		},
+		{
+			"Aasta": 2011,
+			"Koht": 8,
+			"Tootajad": 30025
+		},
+		{
+			"Aasta": 2012,
+			"Koht": 9,
+			"Tootajad": 30348
+		},
+		{
+			"Aasta": 2013,
+			"Koht": 9,
+			"Tootajad": 30717
+		},
+		{
+			"Aasta": 2014,
+			"Koht": 9,
+			"Tootajad": 31001
+		},
+		{
+			"Aasta": 2015,
+			"Koht": 7,
+			"Tootajad": 31117
+		},
+		{
+			"Aasta": 2016,
+			"Koht": 7,
+			"Tootajad": 32792
+		},
+		{
+			"Aasta": 2017,
+			"Koht": 7,
+			"Tootajad": 32831
+		},
+		{
+			"Aasta": 2018,
+			"Koht": 7,
+			"Tootajad": 32831
+		}]
+	},
+    {
+        "Sektor": "Töötlev tööstus",
+		"values": [{
+			"Aasta": 2007,
+			"Koht": 12,
+			"Tootajad": 116159
+		},
+		{
+			"Aasta": 2008,
+			"Koht": 12,
+			"Tootajad": 116159
+		},
+		{
+			"Aasta": 2009,
+			"Koht": 12,
+			"Tootajad": 95854
+		},
+		{
+			"Aasta": 2010,
+			"Koht": 11,
+			"Tootajad": 88123
+		},
+		{
+			"Aasta": 2011,
+			"Koht": 11,
+			"Tootajad": 93779
+		},
+		{
+			"Aasta": 2012,
+			"Koht": 10,
+			"Tootajad": 96091
+		},
+		{
+			"Aasta": 2013,
+			"Koht": 11,
+			"Tootajad": 97546
+		},
+		{
+			"Aasta": 2014,
+			"Koht": 11,
+			"Tootajad": 101893
+		},
+		{
+			"Aasta": 2015,
+			"Koht": 11,
+			"Tootajad": 101882
+		},
+		{
+			"Aasta": 2016,
+			"Koht": 11,
+			"Tootajad": 100129
+		},
+		{
+			"Aasta": 2017,
+			"Koht": 10,
+			"Tootajad": 104915
+		},
+		{
+			"Aasta": 2018,
+			"Koht": 10,
+			"Tootajad": 104915
+		}]
+	},
+    {
+        "Sektor": "Veevarustus; kanalisatsioon; jäätme- ja saastekäitlus",
+		"values": [{
+			"Aasta": 2007,
+			"Koht": 10,
+			"Tootajad": 3270
+		},
+		{
+			"Aasta": 2008,
+			"Koht": 10,
+			"Tootajad": 3270
+		},
+		{
+			"Aasta": 2009,
+			"Koht": 8,
+			"Tootajad": 3388
+		},
+		{
+			"Aasta": 2010,
+			"Koht": 8,
+			"Tootajad": 3117
+		},
+		{
+			"Aasta": 2011,
+			"Koht": 10,
+			"Tootajad": 3117
+		},
+		{
+			"Aasta": 2012,
+			"Koht": 8,
+			"Tootajad": 3107
+		},
+		{
+			"Aasta": 2013,
+			"Koht": 8,
+			"Tootajad": 3108
+		},
+		{
+			"Aasta": 2014,
+			"Koht": 8,
+			"Tootajad": 3330
+		},
+		{
+			"Aasta": 2015,
+			"Koht": 8,
+			"Tootajad": 3035
+		},
+		{
+			"Aasta": 2016,
+			"Koht": 8,
+			"Tootajad": 2996
+		},
+		{
+			"Aasta": 2017,
+			"Koht": 8,
+			"Tootajad": 3266
+		},
+		{
+			"Aasta": 2018,
+			"Koht": 8,
+			"Tootajad": 3266
+		}]
+	},
+    {
+        "Sektor": "Veondus ja laondus",
+		"values": [{
+			"Aasta": 2007,
+			"Koht": 9,
+			"Tootajad": 36964
+		},
+		{
+			"Aasta": 2008,
+			"Koht": 9,
+			"Tootajad": 36964
+		},
+		{
+			"Aasta": 2009,
+			"Koht": 9,
+			"Tootajad": 31151
+		},
+		{
+			"Aasta": 2010,
+			"Koht": 7,
+			"Tootajad": 30099
+		},
+		{
+			"Aasta": 2011,
+			"Koht": 9,
+			"Tootajad": 31815
+		},
+		{
+			"Aasta": 2012,
+			"Koht": 11,
+			"Tootajad": 36103
+		},
+		{
+			"Aasta": 2013,
+			"Koht": 10,
+			"Tootajad": 36434
+		},
+		{
+			"Aasta": 2014,
+			"Koht": 10,
+			"Tootajad": 38918
+		},
+		{
+			"Aasta": 2015,
+			"Koht": 9,
+			"Tootajad": 37651
+		},
+		{
+			"Aasta": 2016,
+			"Koht": 9,
+			"Tootajad": 35801
+		},
+		{
+			"Aasta": 2017,
+			"Koht": 9,
+			"Tootajad": 34411
+		},
+		{
+			"Aasta": 2018,
+			"Koht": 9,
+			"Tootajad": 34411
+		}]
+	}
+]
